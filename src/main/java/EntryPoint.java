c09aca809b4e0aad7b982a87f6c1137af67dd9ca
Bobby Singla <bobbysingla1112@gmail.com>
2019-02-25 10:18:39 +0530

    second commit

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,14 +1,36 @@
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 public class EntryPoint {
+    static TreeMap<String, PlayersStatistics> playerdata;
 
     public static void main(String[] args) {
-        new GeneratePlayers().disp();
-
-
+        EntryPoint entryPoint = new EntryPoint();
+        playerdata = new GenerateData().teamandPlayerdata();
+        System.out.println("Winner is : " + entryPoint.winner(playerdata));
     }
 
+    public String winner(TreeMap<String, PlayersStatistics> playerdata) {
+        if (!(playerdata.isEmpty())) {
+            int Team1 = 0;
+            int Team2 = 0;
+            for (Map.Entry<String, PlayersStatistics> entry : playerdata.entrySet()) {
+                String[] split = entry.getKey().split("-");
+                if (split[0].equals("team1")) {
+                    Team1 = Team1 + entry.getValue().getTotalNoOfKillInGame();
+                } else if (split[0].equals("team2")) {
+                    Team2 += entry.getValue().getTotalNoOfKillInGame();
+                }
+            }
+            if (Team1 > Team2) {
+                return "Team 1 Wins";
+            } else {
+                return "Team 2 Wins";
+            }
+        }
+        return "Both teams have equal no. of kills";
     }
+}
+
+
+
---
+++ src/main/java/GenerateData.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+import java.util.TreeMap;
+
+public class GenerateData {
+    TreeMap<String, PlayersStatistics> playerdata = new TreeMap<String, PlayersStatistics>();
+
+    TreeMap<String, PlayersStatistics> teamandPlayerdata() {
+        for (int i = 0; i < 3; i++) {
+            playerdata.put("Team1-" + "Player" + (i + 1), generateData());
+        }
+        for (int i = 0; i < 3; i++) {
+            playerdata.put("Team2-" + "Player" + (i + 1), generateData());
+        }
+        return playerdata;
+    }
+
+    PlayersStatistics generateData() {
+        Random random = new Random();
+        int totalNoOfAttemptedAttacks = random.nextInt(50);
+        int totalN0OfHits = random.nextInt(totalNoOfAttemptedAttacks);
+        int totalAmountDamageDone = ((totalN0OfHits / totalNoOfAttemptedAttacks) * 100);
+        int totalNoOfKillInGame = random.nextInt(totalN0OfHits);
+        int noOfFirstHitKills = random.nextInt(totalN0OfHits);
+        int noOfAssists = random.nextInt(totalN0OfHits);
+        int noOfSpellCast = random.nextInt(10);
+        int totalSpellCastDamageDone = random.nextInt(15);
+        int totalTimePlayed = random.nextInt(100);
+        int totalNoOfGamesPlayed = random.nextInt(200);
+        int totalDurationOfGamesPlayed = random.nextInt(totalTimePlayed) + 50;
+        int totalNoOfKill = random.nextInt(totalNoOfKillInGame) + 25;
+        int totalGamesWinned = random.nextInt(totalDurationOfGamesPlayed);
+        System.out.println(totalNoOfAttemptedAttacks + "    " + totalN0OfHits
+                + " " + totalAmountDamageDone + " " + totalNoOfKillInGame + " " +
+                noOfFirstHitKills + " " + noOfAssists + " " + noOfSpellCast + " " +
+                totalSpellCastDamageDone + " " + totalTimePlayed + " " + totalNoOfGamesPlayed + " " +
+                totalDurationOfGamesPlayed + " " + totalNoOfKill + " " + totalGamesWinned);
+        return new PlayersStatistics().totalNoOfAttemptedAttacks(totalNoOfAttemptedAttacks)
+                .tot
+
+
+    }
+}
+
