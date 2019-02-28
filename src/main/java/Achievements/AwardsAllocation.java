36de130719f81da29f3eae50aa59a3a5ca872e38
Bobby Singla <bobbysingla1112@gmail.com>
2019-02-28 10:18:11 +0530

    awards commit

--- src/main/java/Achievements/AwardsAllocation.java
+++ src/main/java/Achievements/AwardsAllocation.java
@@ -1,6 +1,5 @@
 package Achievements;
 
-import GenerateDataa.PlayersStatistics;
 
 import java.util.List;
 import java.util.Map;
@@ -16,15 +15,5 @@ public class AwardsAllocation {
         this.awards = awards;
     }
 
-    public Map<String, List<String>> awardscal(Map<String, PlayersStatistics> playerdata) {
 
-        playerdata.forEach((s, playersStatistics) -> {
-            List<String> awardsLists = awards.stream()
-                    .filter(award -> award.validate(playersStatistics))
-                    .map(Objects::toString)
-                    .collect(Collectors.toList());
-            playerawards.put(s, awardsLists);
-        });
-        return playerawards;
-    }
 }
